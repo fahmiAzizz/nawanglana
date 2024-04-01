dbe5a3de8a4fe0e8a771d2f7f96d6242b4d2756c
Fahmi Aziz <fahmiaziz031102@gmail.com>
2024-04-02 06:43:14 +0700

    up gelombang

--- src/components/Bridge.jsx
+++ src/components/Bridge.jsx
@@ -4,7 +4,7 @@ import bridge2 from "../assets/18.jpg"
 
 const Bridge = () => {
     return (
-        <div className='text-white max-w-screen-mobile mx-auto -mt-16 relative z-20'>
+        <div className='text-white max-w-screen-mobile mx-auto -mt-32 relative z-20'>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
                 <path fill="#0099ff" fill-opacity="1" d="M0,64L30,64C60,64,120,64,180,53.3C240,43,300,21,360,42.7C420,64,480,128,540,160C600,192,660,192,720,160C780,128,840,64,900,48C960,32,1020,64,1080,96C1140,128,1200,160,1260,170.7C1320,181,1380,171,1410,165.3L1440,160L1440,320L1410,320C1380,320,1320,320,1260,320C1200,320,1140,320,1080,320C1020,320,960,320,900,320C840,320,780,320,720,320C660,320,600,320,540,320C480,320,420,320,360,320C300,320,240,320,180,320C120,320,60,320,30,320L0,320Z"></path>
             </svg>
